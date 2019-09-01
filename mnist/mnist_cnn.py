a2cd385249b62099e3c7f195ea12d6b6bf5b9942
Kate <49902595+katebrighteyes@users.noreply.github.com>
2019-09-02 00:19:19 +0900

    Update mnist_cnn.py

--- mnist/mnist_cnn.py
+++ mnist/mnist_cnn.py
@@ -7,14 +7,13 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 tf.set_random_seed(777)  # reproducibility
 
-mnist = input_data.read_data_sets("/home/nvidia/MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("/temp/MNIST_data/", one_hot=True)
 # Check out https://www.tensorflow.org/get_started/mnist/beginners for
 # more information about the mnist dataset
 
 # hyper parameters
 learning_rate = 0.001
-#training_epochs = 15
-training_epochs = 3
+training_epochs = 15
 batch_size = 100
 
 # input place holders
