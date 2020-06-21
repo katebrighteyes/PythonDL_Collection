efb58531c9072a0cce8cb73d54fbcee9e46f0d8f
Kate <49902595+katebrighteyes@users.noreply.github.com>
2020-06-22 01:19:15 +0900

    Update mnist_cnn.py

--- mnist/mnist_cnn.py
+++ mnist/mnist_cnn.py
@@ -7,7 +7,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 tf.set_random_seed(777)  # reproducibility
 
-mnist = input_data.read_data_sets("/temp/MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("./temp/MNIST_data/", one_hot=True)
 # Check out https://www.tensorflow.org/get_started/mnist/beginners for
 # more information about the mnist dataset
 
